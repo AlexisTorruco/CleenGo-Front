be5b877bce2177b598577c8a597b6f0734a64ee0
carla Miranda <waleilla99@gmail.com>
2025-12-09 13:52:43 -0300

    actualizacion del dashboard del cliente

--- src/app/components/ProviderDashboard.tsx
+++ src/app/components/ProviderDashboard.tsx
@@ -30,6 +30,11 @@ interface UserProfile {
   birthDate: string;
   profileImgUrl: string;
   phone: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  postalCode?: string;
 }
 
 interface Appointment {
