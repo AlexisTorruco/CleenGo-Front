a52709517fc708a6c5343ae43166b6ed1e9a5eb8
carla Miranda <waleilla99@gmail.com>
2025-12-15 17:35:44 -0300

    dashboard para los chicos

--- src/app/provider/dashboard/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/provider/[id]/page.tsx
-import PublicProviderProfile from '@/app/components/PublicProviderProfile';
-
-export default function ProviderPage({ params }: { params: { id: string } }) {
-  return <PublicProviderProfile providerId={params.id} />;
-}
