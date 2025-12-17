65a4334af1ce36b1a1ce3ca37d4df460efe53ee9
AlexisTorruco <altorrucoc@hotmail.com>
2025-12-17 00:16:31 -0600

    CORRECIÓN PARA DEPLOY, IMPLEMENTACIÓN DE RECUPERAR CONTRASEÑA FUNCIONA

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import ResetPasswordForm from "../components/ResetPasswordForm";
 
 export default function ResetPasswordPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#F7FAFC] text-[#0C2340] px-4">
-      <ResetPasswordForm />
+      <Suspense fallback={null}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 }
