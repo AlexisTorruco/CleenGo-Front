92211a6aed25f3ada57c842ae8793e3c182b87f1
AlexisTorruco <altorrucoc@hotmail.com>
2025-12-17 15:53:35 -0600

    CAMBIOS: navbar funcionando con admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// CleenGo-Front/src/middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value
-  const { pathname } = request.nextUrl
+  const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
 
   // 🔒 Usuario logueado no puede acceder a login / register
   if (
     token &&
-    (pathname.startsWith('/login') ||
-     pathname.startsWith('/register'))
+    (pathname.startsWith("/login") || pathname.startsWith("/register"))
   ) {
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/login/:path*', '/register/:path*'],
-}
+  matcher: ["/login/:path*", "/register/:path*"],
+};
